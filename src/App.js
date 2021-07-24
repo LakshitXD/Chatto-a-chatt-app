9d89cc4c391063eab222af07aa3d8b25bb64eede
LakshitXD <lakshitk6666@gmail.com>
2021-07-24 11:34:03 +0530

    🚧 initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore'; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
